9ec07199105bf441a04b8f4f6f4db99d17a5b109
Robin Wen <blockxyz@gmail.com>
2021-01-24 14:18:11 +0800

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,6 @@
 # Time: 14:11:48
 
 TOKEN="YOUR_TOKEN_HERE"
-mkdir -p /data/monitor/exinpool
-cd /data/monitor/exinpool
-git clone https://github.com/ExinPool/System
 
 cd /data/monitor/exinpool/System/cpu && \cp config.cfg.defaults config.cfg && sed -i "s/ACCESS_TOKEN=YOUR_ACCESS_TOKEN/ACCESS_TOKEN=$TOKEN/g" config.cfg
 cd /data/monitor/exinpool/System/disk && \cp config.cfg.defaults config.cfg && sed -i "s/ACCESS_TOKEN=YOUR_ACCESS_TOKEN/ACCESS_TOKEN=$TOKEN/g" config.cfg
